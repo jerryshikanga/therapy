90c9a710aced4a29091b12e55645b0fb6df8e14a
vincentarisa <31821835+vincentarisa@users.noreply.github.com>
2018-12-16 12:16:23 +0300

    templates update

--- therapy/urls.py
+++ therapy/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 from profiles.views import index
 
@@ -24,4 +26,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("messages/", include("pinax.messages.urls", namespace="pinax_messages")),
     path("profiles/", include("profiles.urls")),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
